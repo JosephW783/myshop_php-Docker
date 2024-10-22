a0f3d8d2d76c7f86da0a2363c8a907c10669ba82
JosephW783 <giuseppe.guerrieri@studioamica.it>
2024-10-22 13:18:05 +0200

    Fourth_iteration

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,24 @@
 FROM php:8.3-apache
 
 # Installa le estensioni PDO e Mysql
-RUN apt-get update \
- && apt-get install -y git zlib1g-dev \
- && docker-php-ext-install pdo pdo_mysql
+RUN apt-get update && apt-get install -y default-mysql-client \
+libfreetype6-dev \
+libjpeg62-turbo-dev \
+libmcrypt-dev \
+libpng-dev \
+zlib1g-dev \
+libxml2-dev \
+libzip-dev \
+libonig-dev \
+graphviz \
+&& docker-php-ext-configure gd \
+&& docker-php-ext-install -j$(nproc) gd \
+&& docker-php-ext-install pdo_mysql \
+&& docker-php-ext-install mysqli \
+&& docker-php-ext-install zip \
+&& docker-php-ext-install sockets \
+&& docker-php-source delete \
+&& curl -sS https://getcomposer.org/installer | php -- \
 # Copio il file della mia appplicazione nella directory del server
 COPY /src/index.php /var/www/html
 
--- src/index.php
+++ src/index.php
@@ -1,14 +1,16 @@
 <?php
 
 $servername= "localhost";
+$dbhost= "mariadb";
 $username= "root";
 $password = "Sandonaci94";
 $dbname = "myshop";
+$charset = 'utf8';
 
 // Metodo per creare la connessione
 try {
     // Creazione della connessione con PDO
-    $dsn = "mysql:host=$servername;dbname=$dbname;charset=utf8mb4"; // Definizione del DSN (Data Source Name)
+     // Definizione del DSN (Data Source Name)
     $options = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Attiva le eccezioni per gli errori
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Imposta il fetch mode come array associativo
@@ -16,21 +18,39 @@ try {
     ];
 
     // Creazione dell'oggetto PDO
-    $pdo = new PDO($dsn, $username, $password);
-
+    $pdo = new PDO('mysql:host=mariadb;dbname=$dbname;charset=utf8', $username, $password);
     // Connessione riuscita
     echo "Connessione al database riuscita!";
-    
-    // Esegui una query di esempio
-    $query = $pdo->query("SELECT * FROM utente");
 
+    // Esegui una query di esempio
+    // $query = $pdo->query("SELECT * FROM utente");
+    
     // Estrazione dei dati
     // while ($row = $query->fetch()) {
         // Stampa ogni riga del risultato (ad esempio)
-    //    echo $row['nome_colonna'] . '<br>';
-    // }
-    
-} catch (PDOException $e) {
-    // Gestione dell'errore di connessione
-    echo 'Errore di connessione al database: ' . $e->getMessage();
+        //    echo $row['nome_colonna'] . '<br>';
+        // }
+        
+    } catch (PDOException $e) {
+        // Gestione dell'errore di connessione
+        echo 'Errore di connessione al database: ' . $e->getMessage();
 }
+// password mariadb 'ciaiciao94'
+
+/*
+$servername = "mariadb";
+$username = "root";
+$password = "ciaociao94";
+$database = "myshop";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+    */
