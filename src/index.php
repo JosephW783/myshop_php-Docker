4bb266471df0c1477d625f36ae1adbb1ac012e9d
JosephW783 <giuseppe.guerrieri@studioamica.it>
2024-10-24 11:51:51 +0200

    Nona-Iterazione

--- src/index.php
+++ src/index.php
@@ -20,15 +20,54 @@ try {
     // Connessione riuscita
     echo "Connessione al database riuscita!";
     
+   $table = 'articolo';
+   $stmt = $pdo->prepare("SELECT * FROM $table");
+   $stmt->execute();
+
+   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+   // Controlla se ci sono risultati
+   if ($results) {
+    // Visualizza i dati in formato HTML
+    echo "<h1>Dati estratti dalla tabella '$table'</h1>";
+    echo "<table border='1'>";
+    echo "<tr>";
+
+    // Mostra le intestazioni delle colonne
+    foreach (array_keys($results[0]) as $column) {
+        echo "<th>" . htmlspecialchars($column) . "</th>";
+    }
+    echo "</tr>";
+
+    // Mostra i dati
+    foreach ($results as $row) {
+        echo "<tr>";
+        foreach ($row as $data) {
+            echo "<td>" . htmlspecialchars($data) . "</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "Nessun dato trovato nella tabella '$table'.";
+}
+}   
+ catch (PDOException $e) {
+// Gestione dell'errore di connessione
+    echo 'Errore di connessione al database: ' . $e->getMessage();
+}
 
 
-    
 
-   
-    
 
-    
-    // Stampa il risultato
+
+
+// password mariadb 'ciaiciao94'
+
+
+
+
+ // Stampa il risultato
    // if ($databaseExists) {
    // echo "Il database '$dbname' esiste.";
    // }   else {
@@ -45,15 +84,3 @@ try {
     //foreach($utenti as $user) {
       //  echo "ID:" . $user['idUtente'] . "- Username:" . $user['username'] . "<br>";
    // }
-}   
- catch (PDOException $e) {
-// Gestione dell'errore di connessione
-    echo 'Errore di connessione al database: ' . $e->getMessage();
-}
-
-
-
-
-
-
-// password mariadb 'ciaiciao94'
